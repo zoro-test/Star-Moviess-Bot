5b75e5d102be1ade91456ec7f1305a08646d35f9
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-05-04 07:16:19 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -620,7 +620,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         free = 536870912 - monsize
         monsize = get_size(monsize)
         free = get_size(free)
-        await query.message.edit_text(
+        await query.message.edit.text(
             text=script.STATUS_TXT.format(total, users, chats, monsize, free),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
