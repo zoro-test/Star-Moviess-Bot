464bd9e177ce2ccd3e92dd2d31afa8949c7d8931
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-05-14 03:20:59 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,6 +15,7 @@ from info import SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_STR
 from utils import temp
 from typing import Union, Optional, AsyncGenerator
 from pyrogram import types
+from main import *
 
 class Bot(Client):
 
