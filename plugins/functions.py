ae66c066ea59ddc4887ed2e9f7c2ca4840962bec
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-05-14 07:16:11 +0530

    Delete functions.py

--- plugins/functions.py
+++
@@ -1,8 +0,0 @@
-from re import findall
-
-def get_urls_from_text(text: str) -> bool:
-    regex = r"""(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]
-                [.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(
-                \([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\
-                ()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))""".strip()
-    return [x[0] for x in findall(regex, str(text))]
