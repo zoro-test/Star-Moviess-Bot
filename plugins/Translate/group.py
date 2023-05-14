ef4a8c02475d253deb2536333448fdbffc23269c
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-05-14 08:00:57 +0530

    Update group.py

--- plugins/Translate/group.py
+++ plugins/Translate/group.py
@@ -1,7 +1,7 @@
 from googletrans import Translator
 from pyrogram import Client, filters
 from plugins.list import list
-from helper.database import find_one
+from database.gtrans_mdb import find_one
 
 @Client.on_message(filters.group & filters.command(["translate"]))
 async def left(client,message):
